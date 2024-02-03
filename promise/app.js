216dd72e8198f8a6f5a31784f811872194fe207b
Bagus Cahyono <baguscy8@gmail.com>
2024-02-04 00:59:23 +0700

    Callback Hell❎

---
+++ promise/app.js
@@ -0,0 +1,66 @@
+// 2 function yg melakukan request
+
+// callback func version
+const requestCallback = (url, success, failure) => {
+  const delay = Math.floor(Math.random() * 4500) + 500;
+  setTimeout(() => {
+    if (delay > 4000) {
+      failure("Connection timeout");
+    } else {
+      success(`Success request data from ${url} (${delay}ms) `);
+    }
+  }, delay);
+};
+
+//run the function
+requestCallback(
+  "movies.com",
+  function (response) {
+    console.log("Success", response);
+    //masalah yang terjadi (callback hell)👎
+    requestCallback(
+      "movies.com",
+      function (response) {
+        console.log("Success", response);
+        requestCallback(
+          "movies.com",
+          function (response) {
+            console.log("Success", response);
+            requestCallback(
+              "movies.com",
+              function (response) {
+                console.log("Success", response);
+              },
+              function (error) {
+                console.log("failure", error);
+              }
+            );
+          },
+          function (error) {
+            console.log("failure", error);
+          }
+        );
+      },
+      function (error) {
+        console.log("failure", error);
+      }
+    );
+  },
+  function (error) {
+    console.log("failure", error);
+  }
+);
+
+// promise func verison
+const requestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delay > 4000) {
+        reject("Connection timeout");
+      } else {
+        resolve(`Success request data from ${url} (${delay}ms) `);
+      }
+    }, delay);
+  });
+};
