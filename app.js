716b74a1a90cf6d915f0f576589800371cce7e4e
Bagus Cahyono <baguscy8@gmail.com>
2024-02-04 00:37:45 +0700

    callback hell ❌

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+// contoh multi thread
 console.log("First");
 setTimeout(() => {
   console.log("sudah masuk memory proses tapi dijalankan 3 detik kedepan");
 }, 3000);
 console.log("Second");
+
+// callback hell
+setTimeout(() => {
+  document.body.style.backgroundColor = "red";
+  setTimeout(() => {
+    document.body.style.backgroundColor = "green";
+    setTimeout(() => {
+      document.body.style.backgroundColor = "blue";
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+// callback hell🙅 promise👌
