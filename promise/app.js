9c5077db409e8d1b170f9a413c2e73f138b92cd4
Bagus Cahyono <baguscy8@gmail.com>
2024-02-04 01:14:56 +0700

    promise

--- promise/app.js
+++ promise/app.js
@@ -13,50 +13,50 @@ const requestCallback = (url, success, failure) => {
 };
 
 //run the function
-requestCallback(
-  "movies.com",
-  function (response) {
-    console.log("Success", response);
-    //masalah yang terjadi (callback hell)👎
-    requestCallback(
-      "movies.com",
-      function (response) {
-        console.log("Success", response);
-        requestCallback(
-          "movies.com",
-          function (response) {
-            console.log("Success", response);
-            requestCallback(
-              "movies.com",
-              function (response) {
-                console.log("Success", response);
-              },
-              function (error) {
-                console.log("failure", error);
-              }
-            );
-          },
-          function (error) {
-            console.log("failure", error);
-          }
-        );
-      },
-      function (error) {
-        console.log("failure", error);
-      }
-    );
-  },
-  function (error) {
-    console.log("failure", error);
-  }
-);
+// requestCallback(
+//   "movies.com",
+//   function (response) {
+//     console.log("Success", response);
+//     //masalah yang terjadi (callback hell)👎
+//     requestCallback(
+//       "movies.com",
+//       function (response) {
+//         console.log("Success", response);
+//         requestCallback(
+//           "movies.com",
+//           function (response) {
+//             console.log("Success", response);
+//             requestCallback(
+//               "movies.com",
+//               function (response) {
+//                 console.log("Success", response);
+//               },
+//               function (error) {
+//                 console.log("failure", error);
+//               }
+//             );
+//           },
+//           function (error) {
+//             console.log("failure", error);
+//           }
+//         );
+//       },
+//       function (error) {
+//         console.log("failure", error);
+//       }
+//     );
+//   },
+//   function (error) {
+//     console.log("failure", error);
+//   }
+// );
 
 // promise func verison
 const requestPromise = (url) => {
   return new Promise((resolve, reject) => {
     const delay = Math.floor(Math.random() * 4500) + 500;
     setTimeout(() => {
-      if (delay > 4000) {
+      if (delay > 3000) {
         reject("Connection timeout");
       } else {
         resolve(`Success request data from ${url} (${delay}ms) `);
@@ -64,3 +64,41 @@ const requestPromise = (url) => {
     }, delay);
   });
 };
+
+//run the function
+requestPromise("movies.com")
+  .then((response) => {
+    console.log("Success", response);
+    //promise juga bisa terjadi (callback hell)👎😅
+    requestPromise("movies.com")
+      .then((response) => {
+        console.log("Success", response);
+        requestPromise("movies.com")
+          .then((response) => {
+            console.log("Success", response);
+            requestPromise("movies.com")
+              .then((response) => {
+                console.log("Success", response);
+                requestPromise("movies.com")
+                  .then((response) => {
+                    console.log("Success", response);
+                  })
+                  .catch((error) => {
+                    console.log("failure", error);
+                  });
+              })
+              .catch((error) => {
+                console.log("failure", error);
+              });
+          })
+          .catch((error) => {
+            console.log("failure", error);
+          });
+      })
+      .catch((error) => {
+        console.log("failure", error);
+      });
+  })
+  .catch((error) => {
+    console.log("failure", error);
+  });
